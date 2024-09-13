9fd50d23c0aa2a27ed0595ff34856508f1bb0264
Symbiotik <ekwunifechris7@gmail.com>
2024-09-13 23:04:21 +0100

    fix nav-bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div>
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
-          <img alt="MongoDB logo" className="h-10 inline" src="https://raw.githubusercontent.com/mongodb-developer/mern-stack-example/603144e25ba5549159d1962601337652a7bfa253/mern/client/src/assets/mongodb.svg"></img>
+          <img alt="MongoDB logo" className="h-10 inline" src="https://ubiqware.net/wp-content/uploads/2022/02/devops-on-white-1-1024x509.png"></img>
         </NavLink>
 
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
